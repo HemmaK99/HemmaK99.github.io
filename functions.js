6056ed0a782f81de37407c5d53281d0cf0add3b2
Hemma Kilawan <hkilawan@gmail.com>
2016-08-11 11:43:38 -0400

    add

--- functions.js
+++ functions.js
@@ -1,3 +1,3 @@
 function popUp() {
-	alert('Warning! This is Hemma"s and she"s a giant green monster who will eat all of your skittles which is basically happiness a lot so you must be careful lol! -Jaime');
+	alert('Warning! Get ready to learn about fashion!')
 }
